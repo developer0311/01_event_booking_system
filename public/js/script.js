6cafbbf470a834b4f3c6223cb4fe0664a01fb423
Diprati Das <dipratidas2004@gmail.com>
2025-04-15 15:52:04 +0530

    update styles for event cards

--- public/js/script.js
+++ public/js/script.js
@@ -15,21 +15,25 @@ const events = [
   },
   {
     title: "Tech Conference",
-    date: "2025-05-01",
+    date: "2025-04-27",
     location: "Silicon Hall",
     image:
       "https://media.assettype.com/analyticsinsight%2F2024-12-16%2Fauhtq9cf%2FMIND-2024.jpg?w=640&auto=format%2Ccompress",
   },
 ];
 
+// --------------------- RENDER EVENT CARDS --------------------- //
+
 function renderEventCards() {
   const container = document.getElementById("eventsContainer");
   container.innerHTML = "";
   events.forEach((ev) => {
     container.innerHTML += `
-      <div class="col-md-12 mb-3">
+      <div class="col-lg-6 mb-3">
         <div class="card">
-          <img src="${ev.image}" class="card-img-top" alt="${ev.title}">
+        <div class="img-container-2">
+        <img src="${ev.image}" class="card-img-top" alt="${ev.title}">
+        </div>
           <div class="card-body">
             <h5 class="card-title">${ev.title}</h5>
             <p class="card-text">Date: ${ev.date}<br>Location: ${
@@ -44,6 +48,9 @@ function renderEventCards() {
   });
 }
 
+
+// --------------------- RENDER AVAILABLE EVENT CARDS --------------------- //
+
 function showEventsForDate(dateStr) {
   const selected = events.filter((ev) => ev.date === dateStr);
   const list = document.getElementById("eventList");
@@ -56,18 +63,22 @@ function showEventsForDate(dateStr) {
 
   selected.forEach((ev) => {
     list.innerHTML += `
-      <div class="card mb-3 available-event-card">
-        <img src="${ev.image}" class="card-img-top" alt="${ev.title}">
+      <div class="card col-sm-6 col-md-4 mb-3 available-event-card">
+      <div class="img-container-1">
+      <img src="${ev.image}" class="card-img-top" alt="${ev.title}">
+      </div>
         <div class="card-body">
           <h5 class="card-title">${ev.title}</h5>
           <p class="card-text">${ev.location}</p>
-          <a href="event-booking.html?event=${encodeURIComponent(ev.title)}&date=${ev.date}" class="btn btn-sm btn-primary">Book Now</a>
+          <a href="event-booking.html?event=${encodeURIComponent(
+            ev.title
+          )}&date=${ev.date}" class="btn btn-sm btn-primary">Book Now</a>
         </div>
       </div>`;
   });
-  
-  
 }
+
+
 
 document.addEventListener("DOMContentLoaded", () => {
   renderEventCards();
